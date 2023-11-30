6d42452ce39a282b289edacf25e8c2a8ab3f0a96
amenasolvd <akureshi.laba@solvd.com>
2023-11-29 19:56:40 -0500

    logger issue solved

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,29 +2,17 @@ package main;
 
 import exceptions.PhoneNoNotValidException;
 import items.Book;
-import items.Item;
-import items.Magazine;
-import items.Newspaper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import peoples.Librarian;
-import peoples.Manager;
 import peoples.Member;
-import peoples.Staff;
 
 public class Main {
     private static final Logger LOGGER = LogManager.getLogger(main.Main.class);
 
     public static void main(String[] args) {
         Member member = new Member("amena","kureshi","amena.k@gmail.com","1565622665");
-        Staff[] staff = new Staff[2];
-        Manager[] manager = new Manager[1];
-        Librarian[] librarian = new Librarian[1];
-        Item[] item = new Item[5];
         Book book1 = new Book("12345","Khalid Hussaini","Fiction","A thousand Splendid Suns","" );
         Book book2 = new Book("12345","Khalid Hussini","Fiction","A thousand Suns", "");
-        Magazine[] magazine = new Magazine[2];
-        Newspaper[] newspaper = new Newspaper[1];
         Library library = new Library();
         try {
             library.addMember(member);
